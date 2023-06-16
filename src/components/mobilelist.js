57eb37598f88a84f6c66d1a5570a7fc6f1f0be44
raja <rajamr9187@gmail.com>
2023-06-16 21:25:49 +0400

    FINAL CHANGES

---
+++ src/components/mobilelist.js
@@ -0,0 +1,37 @@
+import Image1 from "../aseets/download.jpg";
+import Image2 from "../aseets/images (1).jpg";
+import Image3 from "../aseets/images (2).jpg";
+import Image4 from "../aseets/images.jpg";
+
+export const mobiles = [
+    {
+      id: 1,
+      name: "Samsung Galaxy1",
+      price: "150 OMR",
+      model: "S8 Plus",
+      url: Image1,
+    },
+    {
+      id: 2,
+      name: "Samsung Galaxy2",
+      price: "175 OMR",
+      model: "S8 plus",
+      url: Image2,
+    },
+  
+    {
+      id: 3,
+      name: "Samsung Galaxy3",
+      price: "200 OMR",
+      model: "S8 Plus",
+      url: Image3,
+    },
+  
+    {
+      id: 4,
+      name: "Samsung Galaxy4",
+      price: "250 OMR",
+      model: "S8 Plus ",
+      url: Image4,
+    },
+  ];
