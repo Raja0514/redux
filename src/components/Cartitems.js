0df0f68504ad223ec21ac0c3e981113b57621eb5
raja <rajamr9187@gmail.com>
2023-06-17 22:37:40 +0400

    changes

--- src/components/Cartitems.js
+++ src/components/Cartitems.js
@@ -1,17 +1,15 @@
-import { useEffect, useState } from "react";
+
 import { useSelector } from "react-redux";
 function Cartitems() {
-  const [items, setitems] = useState([]);
+ 
   const Cartitems = useSelector((state) => state.reducer);
   //console.log("new card items", Cartitems);
-  useEffect(() => {
-    setitems(Cartitems);
-  }, [items]);
+ 
   return (
     <>
       <h2>Cart Items</h2>
       <div className="itembox">
-        {items.map((list, index) => {
+        {Cartitems.map((list, index) => {
           return (
             <>
               <div className="smallbox">
